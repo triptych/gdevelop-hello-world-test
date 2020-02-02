43b200cb7ebc056b1e01faa2d66b1f748a0f647e
Andrew Wooldridge <triptych@gmail.com>
2020-02-01 21:50:26 -0800

    initial upload

---
+++ export-hello-world/code0.js
@@ -0,0 +1,20 @@
+gdjs.NewSceneCode = {};
+
+gdjs.NewSceneCode.conditionTrue_0 = {val:false};
+gdjs.NewSceneCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.NewSceneCode.eventsList0xb4be0 = function(runtimeScene) {
+
+}; //End of gdjs.NewSceneCode.eventsList0xb4be0
+
+
+gdjs.NewSceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.NewSceneCode.eventsList0xb4be0(runtimeScene);
+return;
+
+}
+gdjs['NewSceneCode'] = gdjs.NewSceneCode;
